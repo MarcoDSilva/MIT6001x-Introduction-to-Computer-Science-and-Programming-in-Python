9eaf21b4e614b2239a5b1a5a2358592c6d030e5d
MarcoDSilva <38049196+MarcoDSilva@users.noreply.github.com>
2020-05-13 23:45:58 +0100

    pset0 solution

---
+++ pset0/ps0.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 23:21:18 2020
+Pset 0 for MIT 6.001
+@author: MarcoSilva
+"""
+
+import math
+
+x = int(input('Enter number x: '))
+y = int(input('Enter number y: '))
+print('X ** y = ', x**y)
+print('log(x) = ', int(math.log2(x)))
